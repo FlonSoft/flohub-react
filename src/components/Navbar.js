18cedd87def4dc0513b11344d96209e4317239ba
Ben Border <ben@bunnbuns.net>
2022-02-28 23:57:53 -0500

    get nav tabs to work like they should

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
-export default class Navbar extends Component {
-    render() {
-        return (
-            <nav className='nav'>
-                <div className='container flex-center'>
-                    <Link className='logo' to='/'><img src='./logo_76.png' alt="flohub Logo" height='38px'></img></Link>
-                    <span className='flex-grow'></span>
-                    <span className='nav-items'>
-                        <Link to="/login" className='btn btn-primary'>Sign in</Link>
+var navTabsData = [
+    {
+        "name": "Home",
+        "path": "/",
+    },
+    {
+        "name": "Comics",
+        "path": "/comics",
+    },
+    {
+        "name": "Videos",
+        "path": "/videos",
+    }
+]
+
+function navTabs(path) {
+    var tmp = []
+    navTabsData.forEach(element => {
+        var isActiveTab = ""
+        if (`${path}` === element.path)
+            isActiveTab = "active"
+        tmp.push(<Link key={element.name} to={element.path} className={`nav-link ${isActiveTab}`}>{element.name}</Link>)
+    });
+    return (
+        tmp
+    );
+}
+
+const Navbar = () => {
+    const pathName = useLocation().pathname
+    return (
+        <nav className='nav'>
+            <div className='container flex-center'>
+                <Link className='logo' to='/'><img src='./logo_76.png' alt="flohub Logo" height='38px'></img></Link>
+                <span className='flex-grow'></span>
+                <span className='nav-items'>
+                    <Link to="/login" className='btn btn-primary'>Sign in</Link>
+                </span>
+            </div>
+            <div className='nav-border'></div>
+            <div className='navbar'>
+                <div className='container'>
+                    <span className='nav-items flex-center'>
+                        {navTabs(pathName)}
                     </span>
                 </div>
-                <div className='nav-border'></div>
-                <div className='navbar'>
-                    <div className='container'>
-                        <span className='nav-items flex-center'>
-                            <Link to='/' className='nav-link active'>Home</Link>
-                            <Link to='/comics' className='nav-link'>Comics</Link>
-                            <Link to='/videos' className='nav-link'>Videos</Link>
-                        </span>
-                    </div>
-                </div>
-            </nav>
-        )
-    }
+            </div>
+        </nav>
+    )
 }
+
+export default Navbar;
