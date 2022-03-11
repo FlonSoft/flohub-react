9feface5eb8620923e23e74f81da5a2f486539aa
Ben Border <ben@bunnbuns.net>
2022-03-10 23:25:07 -0500

    autofocus input

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
             <form>
                 <h1 className='title'>Sign In</h1>
                 <div className='stacked mt-1'>
-                    <input className='input' type='text' placeholder='Username'></input>
+                    <input className='input' type='text' placeholder='Username' autoFocus></input>
                     <input className='input' type='password' placeholder='Password'></input>
                 </div>
                 <div className='mt-1'>
