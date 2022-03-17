339face8227f1c3eb3c3b6243889ddccbf891818
Ben Border <ben@bunnbuns.net>
2022-03-16 22:35:44 -0400

    clean up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ const Navbar = () => {
         <nav className='nav'>
             <div className='container flex-center'>
                 <a className='logo' href='/'><img src='./logo_76.png' alt="flohub Logo" height='38px'></img></a>
-                <span class="uwu">
+                <span class="flex-center-full">
                     <input class="input light search no-border" name="q" id="q" type="search" placeholder="Search"></input>
                 </span>
                 {/* <a class="button search-btn icon with-icon" href="./search.html" title="Search"><img src={searchIconWhite} alt="Search"></img></a> */}
