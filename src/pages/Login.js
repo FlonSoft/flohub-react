018154c9db935bbdcd5686a7439607f7714c7481
Ben Border <ben@bunnbuns.net>
2022-07-11 23:40:11 -0400

    start work on form submit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,51 @@
 import React from 'react'
 
-const Login = () => {
-    document.title = "Sign In - flohub";
-    return (
-        <>
-            <form>
+class Login extends React.Component {
+    componentDidMount() {
+        document.title = 'Sign In - flohub'
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleInputChange(event) {
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(event) {
+        alert(this.state.username)
+        event.preventDefault()
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
                 <h1 className='title'>Sign In</h1>
                 <div className='stacked mt-1'>
-                    <input className='input' type='text' placeholder='Username' autoFocus></input>
-                    <input className='input' type='password' placeholder='Password'></input>
+                    <input name="username" className='input' type='text' placeholder='Username' autoFocus value={this.state.username}
+                        onChange={this.handleInputChange}></input>
+                    <input name="password" className='input' type='password' placeholder='Password' value={this.state.password}
+                        onChange={this.handleInputChange}></input>
                 </div>
                 <div className='mt-1'>
                     <button className='btn primary'>Submit</button>
                 </div>
             </form>
-        </>
-    )
+        );
+    }
 }
 
 export default Login
