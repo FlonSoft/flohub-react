0b9c648e827ab4fde30b5d3835c9d77203011cd9
Ben Border <ben@bunnbuns.net>
2022-09-08 13:37:19 -0400

    check session when page is loaded

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './css/index.css'
@@ -8,6 +8,34 @@ import NoPage from './pages/NoPage'
 import Login from './pages/Login'
 
 export default function App() {
+  function getCookie(name) {
+    const value = `; ${document.cookie}`
+    const parts = value.split(`; ${name}=`)
+    if (parts.length === 2) return parts.pop().split(';').shift()
+  }
+
+  async function checkSess() {
+    if (!getCookie('sess') || getCookie('sess') === '') {
+      return false
+    }
+    fetch('https://api.flohub.xyz/v1/auth', {
+      headers: {
+        authorization: getCookie('sess')
+      }
+    }).then((response) => response.json()).then((data) => {
+      console.log(JSON.stringify(data.status))
+      if (data.status === 'authorized') {
+        return true
+      } else {
+        return false
+      }
+    })
+  }
+
+  useEffect(() => {
+    checkSess()
+  })
+
   return (
     <BrowserRouter>
       <Routes>
